037663363f010897c2daf3eb2464321cb74960ef
L33t-Rose <jphillipjean@gmail.com>
2024-12-11 09:30:16 -0500

    Stuff that I was working on

--- Server.java
+++ Server.java
@@ -38,12 +38,13 @@ public class Server {
                 threads.add(something);
                 something.start();
             } catch (IOException e) {
-                System.err.println("Server failed to start: " + e.getMessage()); //
+                System.err.println("Server failed to start: " + e.getMessage());
+                break;
             }
         }
         try {
             System.out.println("Cleaning up the threads");
-            for(String id:writers.keySet()){
+            for (String id : writers.keySet()) {
                 PrintWriter pw = writers.get(id);
                 pw.println("Game complete! Good job everyone!");
                 pw.flush();
@@ -54,7 +55,15 @@ public class Server {
                 t.join();
             }
         } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 
+    void gameOver() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            System.out.println(e);
         }
     }
 
@@ -70,9 +79,10 @@ public class Server {
             try (
                     BufferedReader in = new BufferedReader(new InputStreamReader(conns.get(id).getInputStream()));) {
                 PrintWriter out = writers.get(id);
+                gameOver();
                 String input = "";
                 while ((input = in.readLine()) != null) {
-                    System.out.println(input+":"+input.startsWith("update"));
+                    System.out.println(input + ":" + input.startsWith("update"));
                     if (input.equals("show")) {
                         out.write(game.getSudokuString());
                         out.flush();
@@ -83,7 +93,7 @@ public class Server {
                             int i = Integer.parseInt(command[1]);
                             int j = Integer.parseInt(command[2]);
                             int num = Integer.parseInt(command[3]);
-                            System.out.println(""+i+","+j+","+num);
+                            System.out.println("" + i + "," + j + "," + num);
                             gameLock.acquire();
                             if (game.enterNumber(i, j, num)) {
                                 out.println("Success");
@@ -95,6 +105,9 @@ public class Server {
                                     pw.write(game.getSudokuString());
                                     pw.flush();
                                     System.out.println("Finished Response");
+                                }
+                                if (game.isBoardFull()) {
+                                    gameOver();
                                 }
                             } else {
                                 System.out.println("Fail");
@@ -114,7 +127,7 @@ public class Server {
                             System.out.println("Finished Response");
                         }
                     } else {
-                        System.out.println("Unknown Command!:"+input);
+                        System.out.println("Unknown Command!:" + input);
                         out.println("Unknown command");
                     }
                 }
@@ -123,19 +136,27 @@ public class Server {
             } catch (Exception e) {
                 System.err.println(e);
             } finally {
-                System.out.println("Exited");
-                conns.remove(id);
-                writers.remove(id);
-                System.out.println(conns);
+                try{
+
+                    System.out.println("Exited");
+                    conns.remove(id).close();;
+                    writers.remove(id).close();
+                    System.out.println(conns);
+                    System.out.println(writers);
+                }catch(IOException e){
+                    System.out.println(e);
+                }
             }
         }
 
     }
+
     public static void main(String[] args) {
         int port = Integer.parseInt(args[0]);
         try {
             Server s = new Server(port);
-            // Since the server shares state with all of the threads we needed a function to spin up the threads within the class itself.
+            // Since the server shares state with all of the threads we needed a function to
+            // spin up the threads within the class itself.
             s.startAcceptingConnections();
         } catch (IOException e) {
             System.err.println("Server failed to start: " + e.getMessage()); // err.println can use to signify diff
